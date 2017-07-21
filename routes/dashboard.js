a1df54bbb12048d76dcd4f5b054bd6cf291999bf
Gelx- <f4lkebert@gmail.com>
2017-07-21 22:42:23 +1200

    Template changes
    Split layout into parts
    Add error template

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,10 +5,14 @@ router.get('/', function(req, res) {
 	if(!req.dashboard) {
 		return res.render('dashboard_error', {error: 'Could not load dashboard data.'});
 	} else {
-		return res.render('dashboard', {data: req.dashboard});
+		return res.render('db_main', {data: req.dashboard});
 	}
 });
 
-router.get('/')
+router.get('/error', function(req, res) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	return res.render('db_error', {error: err});
+});
 
 module.exports = router;
